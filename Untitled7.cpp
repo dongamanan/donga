d3cfdfa245ace9944f4f4272235a514550206842
dongamanan <dongadaya2009@gmail.com>
2025-05-15 14:32:14 +0530

    Add files via upload

---
+++ Untitled7.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,a,b;
+	
+	printf("enter for multication table number:");
+	scanf("%d",&a);
+	
+	
+	 
+	for(i=1;i<=10;i++)
+	{
+		printf("%d * %d =%d\n",a,i,a*i);
+		
+	}
+	
+}
