2008b835ff44f59e87e2a4974316cf79fc70dbab
dongamanan <dongadaya2009@gmail.com>
2025-05-30 15:35:05 +0530

    Add files via upload

---
+++ pr-2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr[100];
+	
+	printf("Enter any string:");
+	gets(arr);
+	
+    int length;
+    
+//    length=strlen(arr);
+//    printf("this string length is=%d",length);
+//    
+    for(int i=1;i=<26;i++)
+    {
+    	printf
+	}
+}
